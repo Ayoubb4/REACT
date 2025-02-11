84c3fa1c32a2411137a3bd14fa30c4c7737aa0ec
Ayoubb4 <ayoubmisbahfat.al19@emiliojimeno.edu.es>
2025-02-11 18:58:26 +0100

    Subida total 11/02/25

--- pokemonproyecto/src/App.js
+++ pokemonproyecto/src/App.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './components/PokemonList';
 import PokemonSearch from './components/PokemonSearch';
 import './App.css';
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Detecta el scroll para mostrar u ocultar el botón de subir
+    const handleScroll = () => {
+      if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Función para desplazar al principio de la página
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
     <div className="app-container">
@@ -28,6 +51,11 @@ function App() {
             </ul>
           </div>
         </div>
+      )}
+
+      {/* Botón de "Subir" */}
+      {isVisible && (
+        <button className="scroll-to-top" onClick={scrollToTop}>↑</button>
       )}
     </div>
   );
